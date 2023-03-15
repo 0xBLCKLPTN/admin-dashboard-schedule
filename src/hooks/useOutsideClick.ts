61af8920a201d16db37955ad87ccfcbd5a2ceb40
Daniil Ermolaev <blcklptn@icloud.com>
2023-03-15 22:50:33 +0500

    Отступы

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -9,8 +9,10 @@ export function useOutsideClick(ref: RefObject<any>, handler: (event: any) => vo
                 }
                 handler(event);
             };
+            
             document.addEventListener("mousedown", listener);
             document.addEventListener("touchstart", listener);
+            
             return () => {
                 document.removeEventListener("mousedown", listener);
                 document.removeEventListener("touchstart", listener);
