bfb2b173c57b4814eef4738eb862ffa37c67f8eb
Daniil Ermolaev <blcklptn@icloud.com>
2023-03-15 22:49:07 +0500

    Разделил импорты для красоты кода 🤔

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,7 +1,9 @@
 import { MainPageProps } from "./MainPage.props";
-import styles from "./MainPage.module.scss";
 import { Text } from "../../components/Text/Text";
 import { Card } from "../../components/Card/Card";
+
+import styles from "./MainPage.module.scss";
+
 
 export const MainPage = (props: MainPageProps): JSX.Element => {
   const arr1: Array<string> = ['1 курс', '2 курс', '3 курс', '4 курс', '5 курс'];
